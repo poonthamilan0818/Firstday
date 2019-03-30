b2c6c761acb9a3fe88ec308c2d5432b9aa819897
poonthamilan0818 <poonthamilan0818@gmail.com>
2019-03-30 07:52:48 +0530

    git bash done

---
+++ src/org/firstclass/IterateValues.java
@@ -0,0 +1,26 @@
+package org.firstclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IterateValues {
+	
+		public static void main(String[] args) {
+			List<Integer> num=new ArrayList<Integer>();
+			num.add(12);
+			num.add(124);
+			num.add(125);
+			num.add(126);
+			num.add(127);
+			num.add(128);
+			for (int i = 0; i < num.size(); i++) {
+				System.out.println(num);
+				
+			}
+			
+		}
+
+
+	}
+
+
